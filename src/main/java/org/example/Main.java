bc0a83a304278877a7e1d8e8c76981842f63430c
sarav <sara.verre@outlook.it>
2025-12-16 09:47:29 +0100

    add print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {}
+    static void main() {
+        System.out.println("New branch");
+    }
 }
